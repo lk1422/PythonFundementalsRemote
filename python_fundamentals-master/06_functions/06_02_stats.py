385fb07a7e9c2ae18ac5fe8c914e10f103e91e68
Lenny Kinsman <notmastermind.tv@gmail.com>
2021-02-04 18:01:55 -0500

    functions lab completed\

--- python_fundamentals-master/06_functions/06_02_stats.py
+++ python_fundamentals-master/06_functions/06_02_stats.py
@@ -6,8 +6,34 @@ Print these values to the console when calling the function.
 
 example_list = [1, 2, 3, 4, 5, 6, 7]
 
-def stats():
-  # define the function here
-  pass
+def stats(n):
+    '''
+    Takes in a multiple ints or floats
+    finds the min max average and sum of the numbers
+    returns all of them in a dict
+    keys are as followed
+    min
+    max
+    sum
+    avg
+    '''
+    z = 0
+    min = 0
+    max = 0
+    sum = 0
+    total = len(n)
+    for y in n:
+        sum += y
+        if z == 0:
+            min = y
+            z = 1
+        if y < min:
+            min = y
+        if y > max:
+            max = y
+    avg = sum/ total
+    x = {"min": min , "max": max , "sum" : sum , "avg" : avg}
+    return x
 
-# call the function below here
+
+print(stats(example_list))
