0d68570f60da10cda80eb1eb28340cf407fcfc1a
Lenny Kinsman <notmastermind.tv@gmail.com>
2021-02-04 17:59:48 -0500

    functions lab completed\

--- python_fundamentals-master/06_functions/06_01_tasks.py
+++ python_fundamentals-master/06_functions/06_01_tasks.py
@@ -13,3 +13,31 @@ Write a script that completes the following tasks.
 # call your functions, passing in the user input as the arguments, and set their output equal to new variables 
 
 # print your new variables to display the results
+def four_or_seven(n):
+    if n%7 or n%4 == 0:
+        x = True
+    else:
+        x = False
+    return x
+def four_and_seven(n):
+    if n%7 and n%4 == 0:
+        x =  True
+    else:
+        x = False
+    return x
+def num_input():
+    x = int(input("Pick a number from 1 - 1,000,000,000"))
+    return x
+
+fos = four_or_seven(num_input())
+fas = four_and_seven(num_input())
+print(fos)
+print(fas)
+##NOT clear if you wanted me to input the same user input into both or if you wanted me to prompt the user input for both
+'''
+But so you know i can do it
+it would look like this
+x = num_input()
+fos = four_or_seven(x)
+fas = four_and_seven(x)
+'''
--- python_fundamentals-master/06_functions/06_03_three_functions.py
+++ python_fundamentals-master/06_functions/06_03_three_functions.py
@@ -3,3 +3,44 @@ Write a program with 3 functions. Each function must call
 at least one other function and use the return value to do something.
 
 '''
+line = input("Enter a line in the form of y = m x + b: ")
+# defines the first 10 values of a line 1- 10 finds y int and x int:
+def line_converter(n):
+	'''
+	Input a linear function in form of y = mx + b spaced out as such
+	y must be represented as y not a numerical value (must be simplified into this form before hand
+	m represents slope
+	b represents the y intercept
+	the values will be turned out as slope, y int
+
+
+
+	'''
+	n = n.split()
+	n.remove("y")
+	n.remove("=")
+	n.remove("+")
+	n.remove("x")
+	slope = int(n[0])
+	y_int = int(n[1])
+	return [slope , y_int]
+def printSlope_yInt():
+	x = line_converter(line)
+	print("slope = " + str(x[0]) + ", Y-int = " + str(x[1]))
+def firsttenpoints(n):
+	'''
+	finds first 10 points
+	'''
+	x = line_converter(n)
+	slope = x[0]
+	y_int = x[1]
+	z = 0
+	points = {0:0, 1:0, 2:0, 3:0, 4:0, 5:0, 6:0, 7:0, 8:0 , 9:0, 10:0}
+	for z in range(0,11):
+		k = (slope*z) + y_int
+		points[z] = k
+		z += 1
+	return points
+print(firsttenpoints(line))
+
+
