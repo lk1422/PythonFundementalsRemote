cf9ecd149036effe124a84c7c2ba8fed9af01f73
Lenny Kinsman <notmastermind.tv@gmail.com>
2021-02-15 22:45:26 -0500

    done

--- python_fundamentals-master/16_variable_arguments/16_01_args.py
+++ python_fundamentals-master/16_variable_arguments/16_01_args.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+def my_print(*args):
+	for i in args:
+		print(i)
+text1 = "hi"
+lizt_strings = ['test1', 'test2']
+my_print(text1, lizt_strings)
--- python_fundamentals-master/16_variable_arguments/16_02_kwargs.py
+++ python_fundamentals-master/16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,7 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+def name_age(**kwargs):
+	for n, a in kwargs.items():
+		print(f"{n} is {a} years young :)")
+name_age(joe=10,len=9)
