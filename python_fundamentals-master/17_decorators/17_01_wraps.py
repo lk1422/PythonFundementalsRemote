bb732a9ab89e88a2f5715231e3712b6cd7c58967
Lenny Kinsman <notmastermind.tv@gmail.com>
2021-02-16 12:36:04 -0500

    done

--- python_fundamentals-master/17_decorators/17_01_wraps.py
+++ python_fundamentals-master/17_decorators/17_01_wraps.py
@@ -2,3 +2,12 @@
 Write a decorator function that wraps text passed to it in a html <p> tag.
 
 '''
+def html_wrap(text):
+    def inner_func():
+        print(f"html<p> {text}")
+    return inner_func
+
+
+text = "hello, Hi, nice to see you"
+htmlwrapper = html_wrap(text)
+htmlwrapper()
--- python_fundamentals-master/17_decorators/17_02_tags.py
+++ python_fundamentals-master/17_decorators/17_02_tags.py
@@ -3,3 +3,13 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+def html_wrap(tag, text):
+    def inner_func():
+        print(f"html<{tag}> {text}")
+    return inner_func
+
+
+text = "hello, Hi, nice to see you"
+tag = 'p'
+htmlwrapper = html_wrap(tag , text)
+htmlwrapper()
